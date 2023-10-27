4ab48d4995ae9b2ba7acf286afe24655472ad1b8
vrjb <vrajcbhavsar0905@gmail.com>
2023-10-27 11:22:26 +0200

    city code

--- app.py
+++ app.py
@@ -15,7 +15,8 @@ def get_weather_data():
               "units": request.form.get('units')}
     
     response= requests.get(url,params=params)
+    city = data['name']
     data =response.json()
-    return f'data: {data}'
+    return f'data: {data}, city: {city}'
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
