b6ef95a561f00cdd6c0b43dceffd2cf7fbe0b83c
vrjb <vrajcbhavsar0905@gmail.com>
2023-10-27 11:30:51 +0200

    correction

--- app.py
+++ app.py
@@ -15,8 +15,8 @@ def get_weather_data():
               "units": request.form.get('units')}
     
     response= requests.get(url,params=params)
-    city = data['name']
     data =response.json()
+    city = data['name']
     return f'data: {data}, city: {city}'
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
